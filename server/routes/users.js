d84321c5b008a9196c35fcf76d308925968e3917
SkanderMsalmi <msalmi.skander@esprit.tn>
2023-05-06 17:23:30 +0100

    ES-6

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var router = express.Router();
-let userController = require('../controllers/user');
+let userController = require("../controllers/user");
+let authController = require("../controllers/auth");
 
-/* GET users listing. */
-router.get("/",userController.myFunction);
+router.post("/inscription", userController.register);
+router.post("/connexion", authController.connexion);
+router.delete("/logout", authController.logout);
+router.get("/currentuser", authController.currentUser);
 
 module.exports = router;
