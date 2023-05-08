65fb18abacc28d1d964f9e9aa367015884598388
SkanderMsalmi <msalmi.skander@esprit.tn>
2023-05-08 20:49:11 +0100

    ES-8

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { SharedModule } from '../shared/shared.module';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent, SignupComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
-    SharedModule
+    SharedModule,
+    FormsModule,
+ReactiveFormsModule
+
   ]
 })
 export class AuthModule { }
---
+++ client/src/app/core/models/user.model.ts
@@ -0,0 +1,6 @@
+export interface User{
+    _id?: string;
+    fullname:string;
+    password?:string;
+    email:string;
+}
