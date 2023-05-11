489449ce34d2f24a0e3ca53eca39e6f63a329d5d
SkanderMsalmi <msalmi.skander@esprit.tn>
2023-05-11 17:20:34 +0100

    fix error messages

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,6 +2,9 @@ const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
 
 exports.register = async (req, res, next) => {
+  if (User.find({ email: req.body.email })) {
+    return res.status(409).json("Email exists already");
+  }
   const newUser = new User({
     email: req.body.email,
     name: req.body.name,
@@ -12,7 +15,11 @@ exports.register = async (req, res, next) => {
     await newUser.save();
     return res.status(200).json(newUser);
   } catch (error) {
-    return res.status(403).json(error);
+    return res
+      .status(403)
+      .json(
+        " An error occurred while trying to signup. Please try again later."
+      );
   }
 };
 
